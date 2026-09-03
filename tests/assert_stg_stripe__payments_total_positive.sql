fe2eb772136134bb33ff0d36bd50ff434db9df53
Xiao (Susie) Bai <52347927+xiao-susie-bai@users.noreply.github.com>
2026-09-03 01:26:45 -0400

    2026-09-02

--- tests/assert_stg_stripe__payments_total_positive.sql
+++ tests/assert_stg_stripe__payments_total_positive.sql
@@ -1,4 +1,5 @@
-
+-- Refunds have a negative amount, so the total amount should always be >= 0. 
+-- So return records where this isn't true to make the test fail. 
 SELECT order_id
        , SUM(amount) AS total_amount
 FROM {{ ref('stg_stripe__payments') }}
